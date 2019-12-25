369d6a979d12284e5325d203bb6126f20a3509f3
Roman Balaban <25555034+onebalaban@users.noreply.github.com>
2019-12-25 18:46:57 +0300

    Initial commit

---
+++ CS/CS/CS/Controllers/DataGridEmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using T845675test.Models;
+
+namespace CS.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataGridEmployeesController : Controller {
+        InMemoryEmployeesDataContext _data;
+
+        public DataGridEmployeesController(IHttpContextAccessor httpContextAccessor, IMemoryCache memoryCache) {
+            _data = new InMemoryEmployeesDataContext(httpContextAccessor, memoryCache);
+        }
+
+        [HttpGet]
+        public IActionResult Get(DataSourceLoadOptions loadOptions) {
+            return Json(DataSourceLoader.Load(_data.Employees, loadOptions));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromForm]string values) {
+            var newEmployee = new Employee();
+            JsonConvert.PopulateObject(values, newEmployee);
+
+            if (!TryValidateModel(newEmployee))
+                return BadRequest();
+
+            _data.Employees.Add(newEmployee);
+            _data.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromForm]int key, [FromForm]string values) {
+            var employee = _data.Employees.First(a => a.ID == key);
+            JsonConvert.PopulateObject(values, employee);
+
+            if (!TryValidateModel(employee))
+                return BadRequest();
+
+            _data.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public void Delete([FromForm]int key) {
+            var employee = _data.Employees.First(a => a.ID == key);
+            _data.Employees.Remove(employee);
+            _data.SaveChanges();
+        }
+    }
+}
---
+++ CS/CS/CS/Models/InMemoryEmployeesDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T845675test.Models
+{
+    public class InMemoryEmployeesDataContext : InMemoryDataContext<Employee>
+    {
+
+        public InMemoryEmployeesDataContext(IHttpContextAccessor contextAccessor, IMemoryCache memoryCache)
+            : base(contextAccessor, memoryCache)
+        {
+        }
+
+        public ICollection<Employee> Employees => ItemsInternal;
+
+
+        protected override IEnumerable<Employee> Source => SampleData.SampleData.DataGridEmployees;
+
+        protected override int GetKey(Employee item) => item.ID;
+
+        protected override void SetKey(Employee item, int key) => item.ID = key;
+    }
+}
